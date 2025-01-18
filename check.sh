43ebb6623ef63cc5aef1fb052ff959acb6050683
William Leader <83631101+wdlea@users.noreply.github.com>
2025-01-18 17:16:06 +1300

    Added unit testing to check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# Format, then check with all possible combinations of features
-cargo fmt && cargo clippy && cargo clippy --all-features && cargo clippy --features train && cargo clippy --features serde
+# Format, then check with all possible combinations of features, finally execute unit tests
+cargo fmt && cargo clippy && cargo clippy --all-features && cargo clippy --features train && cargo clippy --features serde && cargo t --all-features
