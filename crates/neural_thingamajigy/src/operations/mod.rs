4fbe8fbe0008a900f3669b4a99527ab988700f11
William Leader <83631101+wdlea@users.noreply.github.com>
2025-01-29 14:51:44 +1300

    Added normalize

--- crates/neural_thingamajigy/src/operations/mod.rs
+++ crates/neural_thingamajigy/src/operations/mod.rs
@@ -1,4 +1,6 @@
 /// Defines the Exp struct which represents the Exp operation
 mod exp;
+/// Defines the Normalize struct which normalizes a vector passed into it
+mod normalize;
 
-pub use exp::Exp;
+pub use {exp::Exp, normalize::Normalize};
---
+++ crates/neural_thingamajigy/src/operations/normalize.rs
@@ -0,0 +1,80 @@
+use nalgebra::{RealField, SVector};
+
+use crate::Network;
+
+/// Normalizes anything passed into it
+pub struct Normalize;
+
+impl<T: RealField + Copy, const INPUTS: usize> Network<T, INPUTS, INPUTS> for Normalize {
+    fn evaluate(
+        &self,
+        inputs: SVector<T, INPUTS>,
+        _: &impl crate::activators::Activator<T>,
+    ) -> SVector<T, INPUTS> {
+        inputs.normalize()
+    }
+}
+
+#[cfg(feature = "train")]
+use {crate::TrainableNetwork, nalgebra::SMatrix};
+
+#[cfg(feature = "train")]
+impl<T: RealField + Copy, const INPUTS: usize> TrainableNetwork<T, INPUTS, INPUTS> for Normalize {
+    type LayerInputs = SVector<T, INPUTS>;
+
+    type Gradient = ();
+
+    fn evaluate_training(
+        &self,
+        inputs: nalgebra::SVector<T, INPUTS>,
+        _: &impl crate::activators::Activator<T>,
+    ) -> (nalgebra::SVector<T, INPUTS>, Self::LayerInputs) {
+        (inputs.normalize(), inputs)
+    }
+
+    fn get_gradient(
+        &self,
+        layer_inputs: &Self::LayerInputs,
+        output_loss_gradients: nalgebra::SVector<T, INPUTS>,
+        _: &impl crate::activators::Activator<T>,
+    ) -> (Self::Gradient, nalgebra::SVector<T, INPUTS>) {
+        let output = layer_inputs.normalize();
+
+        (
+            (),
+            (SMatrix::identity() - output * output.transpose()) * output_loss_gradients
+                / layer_inputs.norm(),
+        )
+    }
+
+    // noop
+    fn apply_nudge(&mut self, _: Self::Gradient) {}
+}
+
+/// Testing
+mod test {
+    #[test]
+    fn test_normalize() {
+        use core::f32::consts::PI;
+
+        use nalgebra::Vector6;
+
+        use crate::{
+            activators::Sigmoid, operations::normalize::Normalize, Network, TrainableNetwork,
+        };
+
+        let vec = Vector6::new(1f32, 7f32, -4f32, PI, 100f32, -1700f32);
+        let expected = vec.normalize();
+        assert_eq!(Normalize.evaluate(vec, &Sigmoid), expected);
+
+        let (out, inputs) = Normalize.evaluate_training(vec, &Sigmoid);
+        assert_eq!(out, expected);
+
+        // Just making sure it doesn't panic, not too sure how to check this is correct
+        let (_, _) = Normalize.get_gradient(
+            &inputs,
+            Vector6::new(1f32, 1f32, 1f32, 1f32, 1f32, 1f32),
+            &Sigmoid,
+        );
+    }
+}
